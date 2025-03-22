eb1d0cedcac91201173bbaf827851db2d5f9880d
Emrah Özgilik <eozgilik@yandex.com>
2025-03-22 22:44:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '5b789622d06ff42efbb5823dc792b726', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
